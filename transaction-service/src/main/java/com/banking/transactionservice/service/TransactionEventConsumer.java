7a3509ec447b763d73323447a0abfb9c6644b918
Yash Raj <ryashaj6268@gmail.com>
2026-08-12 01:37:02 +0530

    Add fraud detection and transaction event consumer

--- transaction-service/src/main/java/com/banking/transactionservice/service/TransactionEventConsumer.java
+++ transaction-service/src/main/java/com/banking/transactionservice/service/TransactionEventConsumer.java
@@ -1,5 +1,69 @@
 package com.banking.transactionservice.service;
 
+import com.banking.transactionservice.entity.Transaction;
+import com.banking.transactionservice.entity.TransactionStatus;
+import com.banking.transactionservice.repository.TransactionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class TransactionEventConsumer {
+
+    private final TransactionRepository transactionRepository;
+    private final RedisTemplate<String,String> redisTemplate;
+    private final TransactionService transactionService;
+    private static final long OTP_EXPIRY_MINUTES = 5;
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+
+    public void consumeVerificationRequired(@Payload Map<String, Object> payload){
+
+        try{
+            String transactionId = (String) payload.get("transactionId");
+            String accountNumber = (String) payload.get("accountNumber");
+            String reason = (String) payload.get("reason");
+
+            log.info("Verification required - transaction: {} reason: {}", transactionId,reason);
+
+            Transaction transaction = transactionRepository.findById(transactionId)
+                    .orElseThrow(()-> new RuntimeException("transaction not fount" + transactionId));
+
+            if(transaction.getStatus() != TransactionStatus.PROCESSING){
+                log.warn("Transaction {} not processing - skipping",transactionId);
+                return;
+            }
+
+            // Generate 6 digit otp
+            String otp = String.format("%06d", (int) (Math.random() * 900000) + 100000);
+
+            // Store OTP in Redis - expires in 5 minutes
+            String otpKey = "verification:otp" + transactionId;
+            redisTemplate.opsForValue().set(otpKey,otp, OTP_EXPIRY_MINUTES, TimeUnit.MINUTES);
+
+            // Update Status
+            transaction.setStatus(TransactionStatus.PENDING_VERIFICATION);
+            transactionRepository.save(transaction);
+
+            log.info("OTP generated for transaction: {} expures in {} min",transactionId, OTP_EXPIRY_MINUTES);
+
+
+
+        }
+        catch (Exception e){
+
+        }
+
+    }
 
 }
